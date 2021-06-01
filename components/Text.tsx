af37db2113aa74001e02986646afc9f90f11998b
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-06-01 11:25:22 +0800

    components/Text: added props bold & semi bold

--- components/Text.tsx
+++ components/Text.tsx
@@ -4,16 +4,23 @@ import setClassNames from 'classnames'
 interface Props {
   children: ReactNode
   className?: string
+  bold?: boolean
+  semiBold?: boolean
 }
 
 const Text = ({ children }: Props) => {
   return <p>{children}</p>
 }
 
-const Title = ({ children, className }: Props) => {
+const Title = ({ children, className, bold, semiBold }: Props) => {
   return (
     <h1
-      className={setClassNames('font-bold', className ? className : 'text-2xl')}
+      className={setClassNames(
+        'font-normal',
+        bold && 'font-bold',
+        semiBold && 'font-semibold',
+        className ? className : 'text-2xl'
+      )}
     >
       {children}
     </h1>
